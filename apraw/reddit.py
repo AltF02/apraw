27ff5a7939b60ffed18c7d5a474cc35e1eb9d942
DankDumpster <62047267+DankDumpster@users.noreply.github.com>
2020-04-21 21:13:56 +0200

    Update reddit.py

--- apraw/reddit.py
+++ apraw/reddit.py
@@ -2,6 +2,7 @@ import aiohttp
 
 from .subreddits import Subreddits
 
+
 class reddit():
 
     def __init__(self, client_id, client_secret, username, password, user_agent):
@@ -10,3 +11,13 @@ class reddit():
         self.client_id = client_id
         self.client_secret = client_secret
         self.user_agent = user_agent
+
+        self.comment_kind = "t1"
+        self.account_kind = "t2"
+        self.link_kind = "t3"
+        self.message_kind = "t4"
+        self.subreddit_kind = "t5"
+        self.award_kind = "t6"
+
+        self.subreddits = Subreddits(self)
+        self.access_data = None
