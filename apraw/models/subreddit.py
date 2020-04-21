51110e0ac3089cd1662d802eeea5b2e70b61ca5e
DankDumpster <62047267+DankDumpster@users.noreply.github.com>
2020-04-21 22:08:36 +0200

    Update subreddit.py

--- apraw/models/subreddit.py
+++ apraw/models/subreddit.py
@@ -8,12 +8,17 @@ async def fetch_sub_data(name):
     return data
 
 
-class Subreddit:
+class Subreddits:
 
-    def __init__(self, name):
-        self.name = name
-        self.data = asyncio.create_task(fetch_sub_data(name))
+    def __init__(self, reddit):
+        self.reddit = reddit
+        self.data = asyncio.create_task(fetch_sub_data(reddit))
 
     @property
     def icon_img(self):
         return self.data['data']['icon_img']
+
+    async def new(self, limit=25, **kwargs):
+        async for s in self.reddit.get_listing("/subreddits/new", limit, **kwargs):
+            pass
+            # TODO: Finish this
