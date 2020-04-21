0be49d7aaed15e9772e9aa73b25ae85080dbafe9
DankDumpster <62047267+DankDumpster@users.noreply.github.com>
2020-04-21 22:20:59 +0200

    Update redditor.py

--- apraw/models/redditor.py
+++ apraw/models/redditor.py
@@ -1,4 +1,33 @@
+from datetime import datetime
+
+
 class Redditor:
 
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, reddit, data):
+        self.reddit = reddit
+        self.data = data
+
+        self.name = data["name"]
+
+        if "is_suspended" not in data or not data["is_suspended"]:
+            self.id = data["id"]
+            self.is_suspended = False
+            self.created_utc = datetime.utcfromtimestamp(data["created_utc"])
+
+            self.is_employee = data["is_employee"]
+            self.is_friend = data["is_friend"]
+            self.verified = data["verified"]
+            self.is_gold = data['is_gold']
+            self.is_mod = data["is_mod"]
+            self.has_verified_email = data["has_verified_email"]
+
+            self.link_karma = data["link_karma"]
+            self.comment_karma = data["comment_karma"]
+        else:
+            self.is_suspended = True
+
+    def __str__(self):
+        return self.name
+
+    async def message(self, subject, text, from_sr=""):
+        return await self.reddit.message(self.name, subject, text, from_sr)
