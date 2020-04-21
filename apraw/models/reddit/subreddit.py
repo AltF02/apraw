1e987fea4b32fe54edd9720490c9038566c0e518
Matthew <62047267+DankDumpster@users.noreply.github.com>
2020-04-22 00:35:42 +0200

    Update subreddit.py

--- apraw/models/reddit/subreddit.py
+++ apraw/models/reddit/subreddit.py
@@ -7,6 +7,14 @@ class Subreddit:
         self.subreddit = subreddit
 
     @property
+    async def banned(self):
+        return SubredditRelationship(self, "banned")
+
+    @property
+    def contributor(self):
+        return ContributorRelationship(self, "contributor")
+
+    @property
     async def flair(self):
         return SubredditFlair(self)
 
@@ -18,11 +26,22 @@ class Subreddit:
     async def moderator(self):
         return ModeratorRelationship(self, "moderator")
 
+
+class SubredditFlair:
+
+    async def __call__(self):
+        pass
+
+    def __init__(self, subreddit): \
+        self.subreddit = subreddit
+
+
 class SubredditRelationship:
 
     def __init__(self, subreddit, relationship):
         self.relationship = relationship
         self.subreddit = subreddit
+
 
 class SubredditModeration:
 
@@ -30,7 +49,19 @@ class SubredditModeration:
         self.subreddit = subreddit
 
 
+class ContributorRelationship(SubredditRelationship):
+
+    async def leave(self):
+        pass
+
+
 class ModeratorRelationship(SubredditRelationship):
 
     async def __call__(self, redditor=None):
         params = {} if redditor is None else {"user": redditor}
+
+
+class Modmail:
+
+    async def __call__(self, id=None, mark_read=False):
+        pass
