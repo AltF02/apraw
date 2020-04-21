e14353b0b7ce290068fa5f0dce76e671e1dd7dca
Matthew <62047267+DankDumpster@users.noreply.github.com>
2020-04-22 01:32:12 +0200

    Update subreddit.py

--- apraw/models/reddit/subreddit.py
+++ apraw/models/reddit/subreddit.py
@@ -1,7 +1,89 @@
 import aiohttp
 
+from apraw.models.endpoints import API_PATH
+from apraw.models.listing.generator import ListingGenerator
+
 
 class Subreddit:
+    STR_FIELD = "display_name"
+    MESSAGE_PREFIX = "#"
+
+    @staticmethod
+    def _create_or_update(
+            _reddit,
+            allow_images=None,
+            allow_post_crossposts=None,
+            allow_top=None,
+            collapse_deleted_comments=None,
+            comment_score_hide_mins=None,
+            description=None,
+            domain=None,
+            exclude_banned_modqueue=None,
+            header_hover_text=None,
+            hide_ads=None,
+            lang=None,
+            key_color=None,
+            link_type=None,
+            name=None,
+            over_18=None,
+            public_description=None,
+            public_traffic=None,
+            show_media=None,
+            show_media_preview=None,
+            spam_comments=None,
+            spam_links=None,
+            spam_selfposts=None,
+            spoilers_enabled=None,
+            sr=None,
+            submit_link_label=None,
+            submit_text=None,
+            submit_text_label=None,
+            subreddit_type=None,
+            suggested_comment_sort=None,
+            title=None,
+            wiki_edit_age=None,
+            wiki_edit_karma=None,
+            wikimode=None,
+            **other_settings
+    ):
+        # pylint: disable=invalid-name,too-many-locals,too-many-arguments
+        model = {
+            "allow_images": allow_images,
+            "allow_post_crossposts": allow_post_crossposts,
+            "allow_top": allow_top,
+            "collapse_deleted_comments": collapse_deleted_comments,
+            "comment_score_hide_mins": comment_score_hide_mins,
+            "description": description,
+            "domain": domain,
+            "exclude_banned_modqueue": exclude_banned_modqueue,
+            "header-title": header_hover_text,  # Remap here - better name
+            "hide_ads": hide_ads,
+            "key_color": key_color,
+            "lang": lang,
+            "link_type": link_type,
+            "name": name,
+            "over_18": over_18,
+            "public_description": public_description,
+            "public_traffic": public_traffic,
+            "show_media": show_media,
+            "show_media_preview": show_media_preview,
+            "spam_comments": spam_comments,
+            "spam_links": spam_links,
+            "spam_selfposts": spam_selfposts,
+            "spoilers_enabled": spoilers_enabled,
+            "sr": sr,
+            "submit_link_label": submit_link_label,
+            "submit_text": submit_text,
+            "submit_text_label": submit_text_label,
+            "suggested_comment_sort": suggested_comment_sort,
+            "title": title,
+            "type": subreddit_type,
+            "wiki_edit_age": wiki_edit_age,
+            "wiki_edit_karma": wiki_edit_karma,
+            "wikimode": wikimode,
+        }
+
+        model.update(other_settings)
 
     def __init__(self, subreddit):
         self.subreddit = subreddit
@@ -33,7 +115,7 @@ class SubredditFlair:
         pass
 
     def __init__(self, subreddit): \
-        self.subreddit = subreddit
+            self.subreddit = subreddit
 
 
 class SubredditRelationship:
@@ -47,6 +129,15 @@ class SubredditModeration:
 
     def __init__(self, subreddit):
         self.subreddit = subreddit
+        self._stream = None
+
+    async def modqueue(self, only=None, **generator_kwargs):
+        self._handle_only(only, generator_kwargs)
+        return ListingGenerator(
+            self.subreddit._reddit,
+            API_PATH["about_modqueue"].format(subreddit=self.subreddit),
+            **generator_kwargs
+        )
 
 
 class ContributorRelationship(SubredditRelationship):
