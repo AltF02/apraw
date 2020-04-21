7b94034c71069fdf23490a2843774e3873f5a5d0
DankDumpster <62047267+DankDumpster@users.noreply.github.com>
2020-04-21 21:11:31 +0200

    Update reddit.py

--- apraw/reddit.py
+++ apraw/reddit.py
@@ -1,1 +1,12 @@
+import aiohttp
 
+from .subreddits import Subreddits
+
+class reddit():
+
+    def __init__(self, client_id, client_secret, username, password, user_agent):
+        self.username = username
+        self.password = password
+        self.client_id = client_id
+        self.client_secret = client_secret
+        self.user_agent = user_agent
