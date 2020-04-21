e596478d8f982c43afdebc47479d53d0037079dd
Matthew <62047267+DankDumpster@users.noreply.github.com>
2020-04-22 00:26:01 +0200

    Update subreddit.py

--- apraw/models/reddit/subreddit.py
+++ apraw/models/reddit/subreddit.py
@@ -7,15 +7,33 @@ class Subreddit:
         self.subreddit = subreddit
 
     @property
+    async def flair(self):
+        return SubredditFlair(self)
+
+    @property
     async def mod(self):
         return SubredditModeration(self)
 
+    @property
+    async def moderator(self):
+        return ModeratorRelationship(self, "moderator")
+
+class SubredditRelationship:
+
+    def __init__(self, subreddit, relationship):
+        self.relationship = relationship
+        self.subreddit = subreddit
 
 class SubredditModeration:
 
     def __init__(self, subreddit):
         self.subreddit = subreddit
 
-    async def modqueue(self, **kwargs):
+    async def modqueue(self, limit=100, **kwargs):
+        async for
 
 
+class ModeratorRelationship(SubredditRelationship):
+
+    async def __call__(self, redditor=None):
+        params = {} if redditor is None else {"user": redditor}
